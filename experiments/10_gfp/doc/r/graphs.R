4723db86fc4cee287216b7dc11aa9fa356dac62b
Michael Senn <michael@morrolan.ch>
2019-05-26 12:32:05 +0200

    Update graphs of report 10.

--- experiments/10_gfp/doc/r/graphs.R
+++ experiments/10_gfp/doc/r/graphs.R
@@ -5,3 +5,12 @@ ggplot(df, aes(x=minutes, y=od)) +
   labs(y = "OD", x = "Inkubationszeit [min]", title = "Optische Dichte der Bakterienkultur") +
   scale_x_continuous(breaks = seq(min(df$minutes), max(df$minutes), 20)) +
   geom_point()
+
+
+transformation_rates = c(308264, 281809, 293886, 195541)
+mean(transformation_rates)
+sd(transformation_rates)
+
+absorption_rates = c(0.00393, 0.003592, 0.003746, 0.002493)
+mean(absorption_rates)
+sd(absorption_rates)
